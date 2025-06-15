f154921a46fa3c9064cd8489a442f7ab6fe5a682
BeepBopp <115367530+BeepBopp@users.noreply.github.com>
2025-06-15 18:24:21 -0400

    Update app.py

--- app.py
+++ app.py
@@ -2,6 +2,6 @@ import streamlit as st
 home_page = st.Page("home_page.py", title="Home", icon="🛡️")
 cyberbullying_detector = st.Page("cyberbullying_detector.py", title="Cyberbullying Detector", icon="🚨")
 
-pg = st.navigation([cyberbullying_detector])
+pg = st.navigation([home_page, cyberbullying_detector])
 st.set_page_config(page_title="CyberShield", page_icon="🛡️")
 pg.run()
