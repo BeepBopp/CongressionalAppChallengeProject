3e006c58bc74242d34c77ba613f5525f6c8a501a
BeepBopp <115367530+BeepBopp@users.noreply.github.com>
2025-06-22 15:25:05 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,26 @@
 import streamlit as st
-home_page = st.Page("home_page.py", title="Home", icon="🛡️")
-cyberbullying_detector = st.Page("cyberbullying_detector.py", title="Cyberbullying Detector", icon="🚨")
 
-pg = st.navigation([home_page, cyberbullying_detector])
+pages = {
+  st.Page("home_page.py", title="Home", icon="🛡️"),
+  "Tools": [
+    st.Page("cyberbullying_detector.py", title="Cyberbullying Detector", icon="🚨")
+    # Support Therapist
+    # Victim Recommendations
+    # Moderator Recommendations
+  ],
+  "Resources": [
+    # Resources for Victims
+    # Resources for Teachers / Moderators
+    # Resources for Parents
+    # etc.
+  ],
+  "Reach Out": [
+    # About Us
+    # Leave Feedback
+    # Contact Us
+  ],
+}
+
+pg = st.navigation(pages)
 st.set_page_config(page_title="CyberShield", page_icon="🛡️")
 pg.run()
