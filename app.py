fd59c5d16d53cce8d756eb38abacdbe4f4282611
BeepBopp <115367530+BeepBopp@users.noreply.github.com>
2025-07-23 10:52:42 -0400

    Update app.py

--- app.py
+++ app.py
@@ -2,23 +2,22 @@ import streamlit as st
 
 pages = {
   "Welcome": [
-    st.Page("home_page.py", title="Home", icon="🛡️"),
+    st.Page("welcome_pages/home_page.py", title="Home", icon="🛡️"),
     # How to use this app
   ],
   "Tools": [
-    st.Page("cyberbullying_detector.py", title="Cyberbullying Detector", icon="🚨"),
+    st.Page("tools_pages/cyberbullying_detector.py", title="Cyberbullying Detector", icon="🚨"),
     # Support Therapist
-    st.Page("victim_rec.py", title="Recommendations", icon="💡"),
-    # Moderator Recommendations
+    st.Page("tools_pages/recommendations.py", title="Recommendations", icon="💡"), # Add moderator recommendations
   ],
   "Resources": [
-    st.Page("youth_resources.py", title="Youth Resources", icon="🫂"),
-    st.Page("parent_resources.py", title="Parent Resources", icon="👪"),
-    st.Page("mod_resources.py", title="Moderator Resources", icon="🤝"),
+    st.Page("resource_pages/youth_resources.py", title="Youth Resources", icon="🫂"),
+    st.Page("resource_pages/parent_resources.py", title="Parent Resources", icon="👪"),
+    st.Page("resource_pages/mod_resources.py", title="Moderator Resources", icon="🤝"),
   ],
   "Reach Out": [
-    st.Page("about_us.py", title="About Us", icon="💼"),
-    st.Page("leave_feedback.py", title="Leave Feedback", icon="💬"),
+    st.Page("reach_out_pages/about_us.py", title="About Us", icon="💼"),
+    st.Page("reach_out_pages/leave_feedback.py", title="Leave Feedback", icon="💬"),
   ],
 }
 
