5116680f0266243b746863630e27c54f93af9e72
BeepBopp <115367530+BeepBopp@users.noreply.github.com>
2025-08-22 14:02:36 -0400

    Remove other buttons to debug

--- tools_pages/cyberbullying_detector.py
+++ tools_pages/cyberbullying_detector.py
@@ -109,13 +109,10 @@ with tab1:
 
             if label == "cyberbullying":
                 st.write("\nWould you like to check out our other features to cope with this possible cyberbullying?")
-                if st.button("Chat with our AI Therapist to receive help with this situation"):
-                    st.switch_page("tools_pages/recommendations.py")
+                #if st.button("Chat with our AI Therapist to receive help with this situation"):
                 if st.button("Generate potential responses and next steps with our AI Support Bot"):
                     st.switch_page("tools_pages/recommendations.py")
-                if st.button("Moderators: Use our AI Moderator Assistant for possible courses of action"):
-                    st.switch_page("tools_pages/recommendations.py")
-                    
+                ##if st.button("Moderators: Use our AI Moderator Assistant for possible courses of action"):           
 
             # display_feedback_system("text")
         else:
