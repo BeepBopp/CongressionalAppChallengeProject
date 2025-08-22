c11a8adb9180507f14dcc4e71be6c4dc47ff2931
BeepBopp <115367530+BeepBopp@users.noreply.github.com>
2025-08-22 14:04:14 -0400

    Updated recommendations.py file path

--- tools_pages/cyberbullying_detector.py
+++ tools_pages/cyberbullying_detector.py
@@ -111,7 +111,7 @@ with tab1:
                 st.write("\nWould you like to check out our other features to cope with this possible cyberbullying?")
                 #if st.button("Chat with our AI Therapist to receive help with this situation"):
                 if st.button("Generate potential responses and next steps with our AI Support Bot"):
-                    st.switch_page("tools_pages/recommendations.py")
+                    st.switch_page("recommendations.py")
                 ##if st.button("Moderators: Use our AI Moderator Assistant for possible courses of action"):           
 
             # display_feedback_system("text")
