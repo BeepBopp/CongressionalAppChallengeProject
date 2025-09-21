ea07851a0e8c3c2e61aea1714a654fff80038606
BeepBopp <115367530+BeepBopp@users.noreply.github.com>
2025-09-21 18:54:40 -0400

    Update home_page.py

--- pages/home_page.py
+++ pages/home_page.py
@@ -4,3 +4,6 @@ st.title("🛡️ Welcome to Cybershield")
 
 st.markdown("description here TBD")
 st.markdown("blah blah blah")
+
+st.markdown("temporary inspirational music")
+st.video(data = "https://www.youtube.com/watch?v=QGsevnbItdU")
