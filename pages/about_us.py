15691de8bb42dcaaa152648ff6a666cd9c8b01cf
BeepBopp <115367530+BeepBopp@users.noreply.github.com>
2025-09-22 11:34:55 -0400

    Update about_us.py

--- pages/about_us.py
+++ pages/about_us.py
@@ -3,7 +3,7 @@ import streamlit as st
 st.title("💼 About Us")
 
 st.markdown("""
-### Welcome to CyberShield 🛡️
+### Introduction to CyberShield 🛡️
 
 We created **CyberShield** because we kept seeing cyberbullying everywhere—on Discord, Instagram, and even inside our school’s group chats. 
 There isn’t a lot of support for cyberbullying, and victims are often left without someone to talk to, or wondering if they’re overthinking things and aren’t actually being cyberbullied.
