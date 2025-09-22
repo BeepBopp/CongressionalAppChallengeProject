516ca7ebddb0e373fe1f39bdaa4a20533041b72d
BeepBopp <115367530+BeepBopp@users.noreply.github.com>
2025-09-21 20:07:36 -0400

    Update home_page.py

--- pages/home_page.py
+++ pages/home_page.py
@@ -5,7 +5,7 @@ st.title("🛡️ Welcome to CyberShield")
 st.markdown("Welcome to CyberShield!")
 st.markdown("CyberShield is a collection of bots that work together to identify cyberbullying, respond to it, and offer compassionate support to victims who suffer from online harassment. We offer multiple bots that deal with various aspects of cyberbullying:")
 
-st.markdown("CYBERBULLYING DETECTOR: This bot analyzes certain messages (both screenshots and words) to determine if they are considered cyberbullying or not.)
+st.markdown("CYBERBULLYING DETECTOR: This bot analyzes certain messages (both screenshots and words) to determine if they are considered cyberbullying or not.")
 st.markdown("SUPPORT (rAIna): This bot serves as a therapist to cyberbullying victims who just want to vent about their experiences. rAIna helps victims feel heard and not alone, and it also offers coping strategies for victims to ease their minds. No matter the severity of the situation, rAIna is always here to offer unwavering support and listen.")
 st.markdown("RECCOMENDATIONS (cyberAssist): cyberAssist is a supportive chatbot that helps teens respond to online bullying with gentle questions, a short summary of what happened, and clear next steps that are tailored to fit the victim's desires to move forward. this bot helps victims feel more confident in dealing with cyberbullying.")
 st.markdown("MODERATOR (modAI): modAI is a moderating bot that helps moderators identify and assess potential cyberbullying by understanding tone, context, and patterns. it provides clear summaries and next steps while minimizing false alarms. modAI help moderators gain more clarity and makes their job smoother and more reliable")
