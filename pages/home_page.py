83abd9861f284927e46b8299e297c8cf09a7bceb
BeepBopp <115367530+BeepBopp@users.noreply.github.com>
2025-09-22 11:38:02 -0400

    Update home_page.py

--- pages/home_page.py
+++ pages/home_page.py
@@ -1,18 +1,23 @@
 import streamlit as st
 
-st.title("🛡️ Welcome to CyberShield")
+st.markdown("""
+# 🛡️ Welcome to CyberShield"
 
-st.markdown("CyberShield is a collection of bots that work together to identify cyberbullying, respond to it, and offer compassionate support to victims who suffer from online harassment.")
-st.markdown("")
-st.markdown("⬇️How this app helps⬇️")  
-st.markdown("- Empowers teens to respond to online bullying with confidence")  
-st.markdown("- Offers a safe space to share feelings and find coping strategies")  
-st.markdown("- Provides resources and scenarios for learning and growth")  
-st.markdown("")
+CyberShield is a collection of bots that work together to identify cyberbullying, respond to it, and offer compassionate support to victims who suffer from online harassment.
 
-st.markdown("Here is a brief desription of our bots:")
-st.markdown("🚨 CYBERBULLYING DETECTOR: Analyzes certain messages (both screenshots and words) to determine if they are considered cyberbullying or not.")
-st.markdown("❤️ SUPPORT (rAIna): Serves as a therapist to cyberbullying victims who just want to vent about their experiences, and offer coping strategies.")
-st.markdown("💡 RECCOMENDATIONS (cyberAssist): cyberAssist is a supportive chatbot that helps teens respond to online bullying with clear next steps that are tailored to fit the victim's desires to move forward.")
-st.markdown("🔨 MODERATOR (modAI): modAI identify and assess potential cyberbullying by understanding context, patterns, etc while minimizing false alarms.")
-st.markdown("📚 ADDITIONAL RESOURCES: We have also provided digital resources for youths, parents, and moderators. Links and scenarios help kids build coping skills, parents guide with empathy, and moderators create safer online spaces.")
+---
+
+⬇️ How this app helps ⬇️
+- Empowers teens to respond to online bullying with confidence
+- Offers a safe space to share feelings and find coping strategies
+- Provides resources and scenarios for learning and growth
+
+---
+
+Here is a brief description of our bots:
+🚨 CYBERBULLYING DETECTOR: Analyzes certain messages (both screenshots and words) to determine if they are considered cyberbullying or not.
+❤️ SUPPORT (rAIna): Serves as a therapist to cyberbullying victims who just want to vent about their experiences, and offer coping strategies.
+💡 RECCOMENDATIONS (cyberAssist): cyberAssist is a supportive chatbot that helps teens respond to online bullying with clear next steps that are tailored to fit the victim's desires to move forward.
+🔨 MODERATOR (modAI): modAI identify and assess potential cyberbullying by understanding context, patterns, etc while minimizing false alarms.
+📚 ADDITIONAL RESOURCES: We have also provided digital resources for youths, parents, and moderators. Links and scenarios help kids build coping skills, parents guide with empathy, and moderators create safer online spaces.
+""")
