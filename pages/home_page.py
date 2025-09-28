12caa95a94e3df92121d7beea61b44b008aa8df4
BeepBopp <115367530+BeepBopp@users.noreply.github.com>
2025-09-28 18:58:50 -0400

    Update home_page.py

--- pages/home_page.py
+++ pages/home_page.py
@@ -15,9 +15,13 @@ CyberShield is a collection of bots that work together to identify cyberbullying
 ---
 
 Here is a brief description of our bots: 
-🚨 CYBERBULLYING DETECTOR: Analyzes certain messages (both screenshots and words) to determine if they are considered cyberbullying or not. \
-❤️ SUPPORT (rAIna): Serves as a therapist to cyberbullying victims who just want to vent about their experiences, and offer coping strategies. \
-💡 RECCOMENDATIONS (cyberAssist): cyberAssist is a supportive chatbot that helps teens respond to online bullying with clear next steps that are tailored to fit the victim's desires to move forward. \
-🔨 MODERATOR (modAI): modAI identify and assess potential cyberbullying by understanding context, patterns, etc while minimizing false alarms. \
+🚨 CYBERBULLYING DETECTOR: Analyzes certain messages (both screenshots and words) to determine if they are considered cyberbullying or not. 
+
+❤️ SUPPORT (rAIna): Serves as a therapist to cyberbullying victims who just want to vent about their experiences, and offer coping strategies. 
+
+💡 RECCOMENDATIONS (cyberAssist): cyberAssist is a supportive chatbot that helps teens respond to online bullying with clear next steps that are tailored to fit the victim's desires to move forward. 
+
+🔨 MODERATOR (modAI): modAI identify and assess potential cyberbullying by understanding context, patterns, etc while minimizing false alarms. 
+
 📚 ADDITIONAL RESOURCES: We have also provided digital resources for youths, parents, and moderators. Links and scenarios help kids build coping skills, parents guide with empathy, and moderators create safer online spaces.
 """)
