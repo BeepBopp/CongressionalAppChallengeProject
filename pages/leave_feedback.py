312b95cbc52b25834bd15dae0d74fed637ba3752
BeepBopp <115367530+BeepBopp@users.noreply.github.com>
2025-09-28 20:06:03 -0400

    Update leave_feedback.py

--- pages/leave_feedback.py
+++ pages/leave_feedback.py
@@ -1,23 +1,31 @@
 import streamlit as st
+import gspread
+from google.oauth2.service_account import Credentials
 import pandas as pd
-import os
 
 st.title("Leave Feedback 💬")
 
-with st.form("user_input_form"):
+scope = ["https://www.googleapis.com/auth/spreadsheets",
+         "https://www.googleapis.com/auth/drive"]
+
+creds = Credentials.from_service_account_info(
+    st.secrets["gcp_service_account"],  \
+    scopes=scope
+)
+client = gspread.authorize(creds)
+
+SHEET_NAME = "feedback"
+worksheet = client.open(SHEET_NAME).sheet1
+
+with st.form("feedback_form"):
     email = st.text_input("Email Address")
     feedback = st.text_area("Your Feedback")
     submitted = st.form_submit_button("Submit")
-    
-csv_file_path = "feedback.csv"
-
-data = pd.DataFrame(columns=["Email", "Feedback"])
-
-if os.path.exists(csv_file_path) and os.path.getsize(csv_file_path) > 0:
-    data = pd.read_csv(csv_file_path)
 
 if submitted:
-    new_entry = pd.DataFrame([[email, feedback]], columns=["Email", "Feedback"])
-    data = pd.concat([data, new_entry], ignore_index=True)
-    data.to_csv(csv_file_path, index=False)
-    st.success("Thank you! Your feedback is submitted.")
+    if not email.strip() or not feedback.strip():
+        st.warning("Please enter your and email and your feedback.")
+    else:
+        worksheet.append_row([email.strip(), feedback.strip()])
+        st.success("Thank you! Your feedback was submitted.")
+
