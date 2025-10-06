6484a6d5569db7bea6229dfe7e3cf0cf9918e08e
BeepBopp <115367530+BeepBopp@users.noreply.github.com>
2025-10-05 20:13:17 -0400

    Update recommendations.py

--- pages/recommendations.py
+++ pages/recommendations.py
@@ -33,7 +33,6 @@ if "recommendations_messages" not in st.session_state:
 messages = st.session_state.recommendations_messages
 
 st.title("💡 Cyberassist")
-st.set_page_config(layout="wide")
 
 col_main, col_spacer, col_right = st.columns([4, 0.5, 2])
 
