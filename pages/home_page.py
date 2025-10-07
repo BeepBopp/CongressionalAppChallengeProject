10cbf5907155b0bb10018cc46c526a27b878908f
BeepBopp <115367530+BeepBopp@users.noreply.github.com>
2025-10-07 12:03:29 -0400

    Update home_page.py

--- pages/home_page.py
+++ pages/home_page.py
@@ -19,13 +19,13 @@ Here is a brief description of our bots:
 
 
 🚨  [CYBERBULLYING DETECTOR](https://cybershield.streamlit.app/cyberbullying_detector)
-: Analyzes certain messages (both screenshots and words) to determine if they are considered cyberbullying or not. 
+: Analyzes certain messages (both screenshots and words) to determine if they are considered cyberbullying or not along with clear, easy-to-understand reasoning.
 
-❤️ [SUPPORT (rAIna)](https://cybershield.streamlit.app/therapist): Serves as a therapist to cyberbullying victims who just want to vent about their experiences, and offer coping strategies. 
+❤️ [SUPPORT (rAIna)](https://cybershield.streamlit.app/therapist): Serves as a therapist to offer strategies and support to cyberbullying victims or to anyone who just want to vent about their experiences.
 
 💡 [RECOMMENDATIONS (Cyberassist)](https://cybershield.streamlit.app/recommendations): Cyberassist is a supportive chatbot that helps teens respond to online bullying with clear next steps that are tailored to fit the victim's desires to move forward. 
 
-🔨 [MODERATOR (modAI)](https://cybershield.streamlit.app/moderators): modAI identifies and assess potential cyberbullying by understanding context, patterns, etc while minimizing false alarms. 
+🔨 [MODERATOR (modAI)](https://cybershield.streamlit.app/moderators): modAI identifies and assess potential cyberbullying by understanding context, patterns, etc while minimizing false alarms and provides moderators reports personalized for their situation and needs.
 
 📚 ADDITIONAL RESOURCES: We have also provided digital resources for [youth](https://cybershield.streamlit.app/youth_resources), [parents](https://cybershield.streamlit.app/parent_resources), and [moderators]((https://cybershield.streamlit.app/mod_resources)). Links and scenarios help kids build coping skills, parents guide with empathy, and moderators create safer online spaces.
 """)
